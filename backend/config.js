461706508227246a93657a33e50e4987fa70de70
manishpatil2003 <pmonu5214@gmail.com>
2023-12-28 19:54:46 +0530

    ADDED

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+  'mongodb+srv://manish:131225mp@book-store-mern.wdqcyge.mongodb.net/books-collection';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
